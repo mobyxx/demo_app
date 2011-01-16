04e2719858c7999fa9d19dd98c11c85442f9c5ce
moby <rhabal@pomogo.com>
2011-01-16 01:45:17 -0800

    micropost

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 DemoApp::Application.routes.draw do
-  resources :users
+  resources :microposts
+
+   resources :users
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
